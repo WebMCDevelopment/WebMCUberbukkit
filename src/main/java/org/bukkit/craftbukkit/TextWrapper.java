a5ab40ada6da74a0f48b3a3aa576578f3c4b728f
shark <143028304+sshshark@users.noreply.github.com>
2025-03-08 18:41:11 -0500

    Merge pull request #138 from retromcorg/TextPixelWidthMethod
    Add widthInPixels function

--- src/main/java/org/bukkit/craftbukkit/TextWrapper.java
+++ src/main/java/org/bukkit/craftbukkit/TextWrapper.java
@@ -1,11 +1,28 @@
 package org.bukkit.craftbukkit;
 
 public class TextWrapper {
-    private static final int[] characterWidths = new int[] { 1, 9, 9, 8, 8, 8, 8, 7, 9, 8, 9, 9, 8, 9, 9, 9, 8, 8, 8, 8, 9, 9, 8, 9, 8, 8, 8, 8, 8, 9, 9, 9, 4, 2, 5, 6, 6, 6, 6, 3, 5, 5, 5, 6, 2, 6, 2, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 2, 2, 5, 6, 5, 6, 7, 6, 6, 6, 6, 6, 6, 6, 6, 4, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 4, 6, 4, 6, 6, 3, 6, 6, 6, 6, 6, 5, 6, 6, 2, 6, 5, 3, 6, 6, 6, 6, 6, 6, 6, 4, 6, 6, 6, 6, 6, 6, 5, 2, 5, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 4, 6, 3, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 4, 6, 6, 3, 6, 6, 6, 6, 6, 6, 6, 7, 6, 6, 6, 2, 6, 6, 8, 9, 9, 6, 6, 6, 8, 8, 6, 8, 8, 8, 8, 8, 6, 6, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 6, 9, 9, 9, 5, 9, 9, 8, 7, 7, 8, 7, 8, 8, 8, 7, 8, 8, 7, 9, 9, 6, 7, 7, 7, 7, 7, 9, 6, 7, 8, 7, 6, 6, 9, 7, 6, 7, 1 };
-    private static final char COLOR_CHAR = '\u00A7';
-    private static final int CHAT_WINDOW_WIDTH = 320;
-    private static final int CHAT_STRING_LENGTH = 119;
-    private static final String allowedChars = net.minecraft.server.FontAllowedCharacters.allowedCharacters;
+    private static final int[] characterWidths = new int[] {
+        1, 9, 9, 8, 8, 8, 8, 7, 9, 8, 9, 9, 8, 9, 9, 9,
+        8, 8, 8, 8, 9, 9, 8, 9, 8, 8, 8, 8, 8, 9, 9, 9,
+        4, 2, 5, 6, 6, 6, 6, 3, 5, 5, 5, 6, 2, 6, 2, 6,
+        6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 2, 2, 5, 6, 5, 6,
+        7, 6, 6, 6, 6, 6, 6, 6, 6, 4, 6, 6, 6, 6, 6, 6,
+        6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 4, 6, 4, 6, 6,
+        3, 6, 6, 6, 6, 6, 5, 6, 6, 2, 6, 5, 3, 6, 6, 6,
+        6, 6, 6, 6, 4, 6, 6, 6, 6, 6, 6, 5, 2, 5, 7, 6,
+        6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 4, 6, 3, 6, 6,
+        6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 4, 6,
+        6, 3, 6, 6, 6, 6, 6, 6, 6, 7, 6, 6, 6, 2, 6, 6,
+        8, 9, 9, 6, 6, 6, 8, 8, 6, 8, 8, 8, 8, 8, 6, 6,
+        9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9,
+        9, 9, 9, 9, 9, 9, 9, 9, 9, 6, 9, 9, 9, 5, 9, 9,
+        8, 7, 7, 8, 7, 8, 8, 8, 7, 8, 8, 7, 9, 9, 6, 7,
+        7, 7, 7, 7, 9, 6, 7, 8, 7, 6, 6, 9, 7, 6, 7, 1
+    };
+    public static final char COLOR_CHAR = '\u00A7';
+    public static final int CHAT_WINDOW_WIDTH = 320;
+    public static final int CHAT_STRING_LENGTH = 119;
+    public static final String allowedChars = net.minecraft.server.FontAllowedCharacters.allowedCharacters;
 
     public static String[] wrapText(final String text) {
         final StringBuilder out = new StringBuilder();
@@ -67,6 +84,40 @@ public class TextWrapper {
 
         // Return it split
         return out.toString().split("\n");
+    }
+
+    /**
+     * Calculates the width of a string in pixels based on Minecraft's character widths.
+     * The maximum width for chat is 320 pixels (Use CHAT_WINDOW_WIDTH).
+     *
+     * @param string The input string.
+     * @return The width of the string in pixels.
+     */
+    public static int widthInPixels(final String text) {
+        if (text == null || text.isEmpty())
+            return 0;
+
+        int output = 0;
+
+        // literally yoinked from above and removed unnecessary components.
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+
+            if (ch == COLOR_CHAR && i < text.length() - 1) {
+                i++;
+                continue;
+            }
+
+            int index = allowedChars.indexOf(ch);
+            if (index == -1)
+                continue;
+
+            index += 32; // compensate for gap in allowed characters
+
+            output += characterWidths[index];
+        }
+
+        return output;
     }
 
     // uberbukkit
